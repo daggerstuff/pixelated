b94349da19c01448405b8a85f466aff9ae1f111a
Dagger <dagger@pixelated.love>
2026-04-13 20:45:44 +0000

    fix: address review comment on src/utils/toc.test.ts

--- src/utils/toc.test.ts
+++ src/utils/toc.test.ts
@@ -6,7 +6,7 @@ describe('generateToc', () => {
   it('should throw an error if minHeadingLevel > maxHeadingLevel', () => {
     const minHeadingLevel: HeadingLevel = 3
     const maxHeadingLevel: HeadingLevel = 2
-    expect(() => generateToc([], minHeadingLevel, maxHeadingLevel)).toThrow(/minHeadingLevel.*maxHeadingLevel/)
+    expect(() => generateToc([], minHeadingLevel, maxHeadingLevel)).toThrowError('`minHeadingLevel` must be less than or equal to `maxHeadingLevel`')
   })
 
   it('should filter headings and build a hierarchical ToC', () => {
@@ -34,10 +34,12 @@ describe('generateToc', () => {
     ]
     const minHeadingLevel: HeadingLevel = 1
     const maxHeadingLevel: HeadingLevel = 6
-
     const toc = generateToc(headings, minHeadingLevel, maxHeadingLevel)
 
-    expect(toc[0].children[0].slug).toBe('') // filler for level 3
+    expect(toc).toHaveLength(1)
+    expect(toc[0].text).toBe('H2')
+    expect(toc[0].children).toHaveLength(1)
+    expect(toc[0].children[0].children).toHaveLength(1)
     expect(toc[0].children[0].children[0].text).toBe('H4')
   })
 })
