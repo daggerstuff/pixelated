c3887c3c8966146d8717ca22733b99b931a90fd6
Dagger <antigravity@google.com>
2026-04-26 02:13:09 +0000

    chore: remove legacy UI components and layouts (Brutalist, Tailus, Mizu, Vitesse)

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,5 +1,3 @@
-export { default as Footer } from './Footer.astro'
-
 export { default as HeaderReact } from './HeaderReact'
 export type { HeaderProps } from './HeaderReact'
 export { default as Navigation } from './Navigation'
