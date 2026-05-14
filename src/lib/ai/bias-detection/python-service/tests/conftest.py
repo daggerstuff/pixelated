1443253b9b32f3615627688623890be84f9c7921
Dagger <antigravity@google.com>
2026-05-14 02:31:56 +0000

    auto-commit: 2026-05-14 02:31:56

---
+++ src/lib/ai/bias-detection/python-service/tests/conftest.py
@@ -0,0 +1,14 @@
+import warnings
+
+
+warnings.filterwarnings(
+    "ignore",
+    message=r"\s*Eventlet is deprecated.*",
+    category=Warning,
+)
+warnings.filterwarnings(
+    "ignore",
+    message=r"The default value of `allowed_objects` will change in a future version.*",
+    category=Warning,
+)
+
