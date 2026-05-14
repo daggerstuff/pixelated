06cf32474df6c70c75f400fddaeb3c6c834170d5
Dagger <antigravity@google.com>
2026-05-14 02:53:43 +0000

    auto-commit: 2026-05-14 02:53:43

--- src/lib/ai/bias-detection/python-service/sitecustomize.py
+++ src/lib/ai/bias-detection/python-service/sitecustomize.py
@@ -5,8 +5,17 @@ import warnings
 
 if "pytest" in os.path.basename(sys.argv[0]):
     warnings.filterwarnings("ignore", message=r"\s*Eventlet is deprecated.*", category=Warning)
-    warnings.filterwarnings(
-        "ignore",
-        message=r"The default value of `allowed_objects` will change in a future version.*",
-        category=Warning,
-    )
+    try:
+        from langchain_core._api.deprecation import LangChainPendingDeprecationWarning
+    except Exception:
+        warnings.filterwarnings(
+            "ignore",
+            message=r"The default value of `allowed_objects` will change in a future version.*",
+            category=Warning,
+        )
+    else:
+        warnings.filterwarnings(
+            "ignore",
+            message=r"The default value of `allowed_objects` will change in a future version.*",
+            category=LangChainPendingDeprecationWarning,
+        )
