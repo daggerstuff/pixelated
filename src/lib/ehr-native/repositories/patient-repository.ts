11dc8521126d65dab85064df3a7e279736c97690
Steiner <dagger@pixelated.love>
2026-08-22 19:41:03 -0400

    style: fix prettier formatting (single quotes)

--- src/lib/ehr-native/repositories/patient-repository.ts
+++ src/lib/ehr-native/repositories/patient-repository.ts
@@ -114,7 +114,7 @@ export class PatientRepository extends BaseRepository<Patient> {
     limit = 20,
     offset = 0,
   ): Promise<Patient[]> {
-    const escapedName = nameQuery.replace(/[%_\\]/g, "\\$&")
+    const escapedName = nameQuery.replace(/[%_\\]/g, '\\$&')
     const pattern = `%${escapedName}%`
     return this.withRLS(async (client) => {
       const res = await client.query<{ fhir_resource: Patient }>(
